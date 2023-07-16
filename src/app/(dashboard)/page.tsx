88a1db95ad305147949bf7230a5092d2f2c065d6
RABrL <robin_brito@hotmail.com>
2023-07-16 18:17:20 -0500

    Change page index location

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 export default async function Index () {
   return (
     <div className='flex-1 flex flex-col max-w-3xl mt-24'>
-      <h1>hola mundo</h1>
+      <h1>Dashboard</h1>
     </div>
   )
 }
