d08de00bb4e32bc8c50468ed94084e953f9c51c0
RABrL <robin_brito@hotmail.com>
2023-07-17 16:06:19 -0500

    Add new Button component

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react'
-import { Slot } from '@radix-ui/react-slot'
 import { cva, type VariantProps } from 'class-variance-authority'
-
+import { Ring } from '@uiball/loaders'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
@@ -36,18 +35,21 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
   VariantProps<typeof buttonVariants> {
-  asChild?: boolean
+  isLoading?: boolean
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button'
+  ({ className, children, variant, isLoading, size, ...props }, ref) => {
     return (
-      <Comp
+      <button
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
+        disabled={isLoading}
         {...props}
-      />
+      >
+        {isLoading ? <Ring speed={2} size={22} color='black' lineWeight={5} /> : null}
+        {children}
+      </button>
     )
   }
 )
